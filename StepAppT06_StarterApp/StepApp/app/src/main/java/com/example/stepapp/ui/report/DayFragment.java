68d2f41dab0cfcf8db45eda4de06a0ecb4cc16cf
Felix Boelter <61283564+felixboelter@users.noreply.github.com>
2020-11-28 13:00:15 +0100

    init

---
+++ StepAppT06_StarterApp/StepApp/app/src/main/java/com/example/stepapp/ui/report/DayFragment.java
@@ -0,0 +1,94 @@
+package com.example.stepapp.ui.report;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+import com.example.stepapp.R;
+import com.example.stepapp.StepAppOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class DayFragment extends Fragment {
+
+    AnyChartView anyChartView;
+    public Map<String, Integer> stepsByHour = null;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_day, container, false);
+
+        // Create column chart
+        anyChartView = root.findViewById(R.id.dayBarChart);
+        anyChartView.setProgressBar(root.findViewById(R.id.loadingBar2));
+
+        Cartesian cartesian = createColumnChart();
+        anyChartView.setBackgroundColor("#00000000");
+        anyChartView.setChart(cartesian);
+
+        return root;
+    }
+
+    public Cartesian createColumnChart(){
+        //Read data from SQLiteDatabase
+        stepsByHour = StepAppOpenHelper.loadStepsByDay(getContext());
+
+        // Create and get the cartesian coordinate system for column chart
+        Cartesian cartesian = AnyChart.column();
+
+        // Create data entries for x and y axis of the graph
+        List<DataEntry> data = new ArrayList<>();
+
+        for (Map.Entry<String,Integer> entry : stepsByHour.entrySet())
+            data.add(new ValueDataEntry(entry.getKey(), entry.getValue()));
+
+        // Set the data for column chart and get the columns
+        Column column = cartesian.column(data);
+
+        //Modify the UI of the Column
+        //Change the color of bar chart and its border
+        column.fill("#1EB980");
+        column.stroke("#1EB980");
+
+        // Add tooltip to the bar charts and modify its properties
+        column.tooltip()
+                .titleFormat("Day: {%X}")
+                .position(Position.RIGHT_TOP)
+                .anchor(Anchor.RIGHT_TOP)
+                .offsetX(0d)
+                .offsetY(5)
+                .format("{%Value}{groupsSeparator: } Steps");
+
+
+        // Modify the UI of the Cartesian
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+        cartesian.background().fill("#00000000");
+        cartesian.yScale().minimum(0);
+        cartesian.yAxis(0).title("Number of steps");
+        cartesian.xAxis(0).title("Day");
+        cartesian.animation(true);
+
+        return cartesian;
+    }
+}
